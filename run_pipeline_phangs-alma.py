f5223aaa00bd07cb36406650b485ab8e1f9501d7
Ashley Barnes <30494539+ashleythomasbarnes@users.noreply.github.com>
2026-07-28 15:49:31 +0200

    skip staging

--- run_pipeline_phangs-alma.py
+++ run_pipeline_phangs-alma.py
@@ -18,7 +18,7 @@ master_key_file = "/nfs/home/abarnes/PHANGS/ALMA/phangs_imaging_scripts/phangs-a
 
 # Steps to run
 do_singledish = False
-do_staging = True
+do_staging = False
 do_imaging = True
 do_postprocess = True
 do_derived = False
